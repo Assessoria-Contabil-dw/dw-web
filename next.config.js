ee1d9b0c2acb4463e914d80aeb3dbbe7f06afd56
laillagaleno <laillagalenoitz@gmail.com>
2024-03-24 22:17:22 -0300

    next.confing

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ module.exports = {
   //     // },
   //   ]
   // },
-
   output: "standalone",
   images: {
     remotePatterns: [
@@ -27,19 +26,5 @@ module.exports = {
         pathname: '/dfvdw6xvy/image/upload/**',
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: process.env.NEXT_PUBLIC_API_URL + "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
   },
 }
